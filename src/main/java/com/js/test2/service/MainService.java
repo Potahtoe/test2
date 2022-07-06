2cca37e618e6b44df8cb9b212bba3dd4fd04f22b
DESKTOP-M96VU48\user <potatok1336@naver.com>
2022-07-06 10:59:42 +0900

    no message

--- src/main/java/com/js/test2/service/MainService.java
+++
@@ -1,9 +0,0 @@
-package com.js.test2.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.ui.Model;
-
-public interface MainService {
-	public void leftList(HttpServletRequest req, Model model);
-}
